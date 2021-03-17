458d2cd6b276304ccf5cdec9486c1648fc46be80
Elena Erofeeva <eerofeeva8@gmail.com>
2021-03-17 01:54:12 -0500

    WIP: removed Instructions

--- Instructions/static/js/config.js
+++
@@ -1,3 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1Ijoic2Nob29sZWEiLCJhIjoiY2ttYnNma3I2MDEwMDJ5bXhuYmYzeXJhdyJ9.Ks3omqLhzRrH5bByrWueJQ";
-const Mapbox_API_KEY = "pk.eyJ1Ijoic2Nob29sZWEiLCJhIjoiY2trNjVuOXRuMDBxYzJ2bzdsOGVheDhhYSJ9.7RGGtTdaPtiCmofmLFBa6g";
